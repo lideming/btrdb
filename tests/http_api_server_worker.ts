43c4e200f9e1450b5fd43702f7cee331e1db8241
lideming <me@yuuza.net>
2023-02-07 09:59:58 +0800

    test http api server w/o worker

--- tests/http_api_server_worker.ts
+++ tests/http_api_server_worker.ts
@@ -1,18 +1,26 @@
 import { Database, HttpApiServer } from "../mod.ts";
 
-export async function startTestServer() {
-  try {
-    await Deno.remove("testdata/httptest.db");
-  } catch {}
-  const db = await Database.openFile("testdata/httptest.db");
-  // const db = await Database.openMemory();
+export async function startTestServer(port = 1234) {
+  // const db = await Database.openFile("testdata/httptest.db");
+  const db = await Database.openMemory();
   (self as any).postMessage?.({ msg: "ready" });
   db.autoCommit = true;
-  new HttpApiServer(db).serve(
-    Deno.listen({ hostname: "127.0.0.1", port: 1234 }),
-  );
+  const listener = Deno.listen({ hostname: "127.0.0.1", port });
+  const serveTask = new HttpApiServer(db).serve(listener);
+  return async () => {
+    db.close();
+    listener.close();
+    await serveTask;
+  };
 }
 
 if ((self as any).postMessage) {
-  startTestServer();
+  const close = await startTestServer();
+  (self as any).addEventListener("message", (e: MessageEvent) => {
+    if (e.data.msg === "close") {
+      close().then(() => {
+        (self as any).postMessage({ msg: "closed" });
+      });
+    }
+  });
 }
